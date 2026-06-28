b72dd47dadeabf5974e1a3c28ec4d3310a717436
Mohan <maddukurimohan98@gmail.com>
2026-06-28 15:38:58 +0530

    Add service layer with construction injection

--- banking-system/src/main/java/com/banking/banking_system/controller/CustomerController.java
+++ banking-system/src/main/java/com/banking/banking_system/controller/CustomerController.java
@@ -3,11 +3,21 @@ package com.banking.banking_system.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.banking.banking_system.service.CustomerService;
+
 @RestController
 public class CustomerController {
 
+	private final CustomerService customerService;
+	
+	public CustomerController(CustomerService customerService) {
+		this.customerService = customerService;
+		
+	}
+	
+	
 	@GetMapping("/api/v1/customers")
 	public String getCustomers() {
-		return "Welcome to Banking Management System";
+		return customerService.getCustomers();
 	}
 }
---
+++ banking-system/src/main/java/com/banking/banking_system/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.banking.banking_system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+	public String getCustomers() {
+		return "Welcome to Banking Management System";
+	}
+}
